84569bfabc9990a33dc2ef2e500db5af8f1ab49a
Adeola <adeola.adeyemoj@yahoo.com>
2022-02-16 11:35:11 +0100

    chore: add formatting and linting configuration to the project
    Signed-off-by: Adeola <adeola.adeyemoj@yahoo.com>

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'property-no-unknown': null,
+    'custom-property-no-missing-var-function': null,
+    'block-no-empty': null,
+    'function-no-unknown': null,
+    'unit-no-unknown': null,
+  },
+};
