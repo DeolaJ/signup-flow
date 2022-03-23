da0544a412c32d55bbbeb92b5c240e495fdb863d
Adeola <adeola.adeyemoj@yahoo.com>
2022-03-23 16:52:44 +0100

    update: remove unused color variables
    Signed-off-by: Adeola <adeola.adeyemoj@yahoo.com>

--- src/components/Auth/Shared/AuthSwitch/auth-switch.styled.ts
+++ src/components/Auth/Shared/AuthSwitch/auth-switch.styled.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const AuthSwitchContainer = styled.p`
   margin: 0;
-  color: var(--color-grey-lighter);
   font-weight: 400;
   font-size: 0.65rem;
   line-height: 1.5rem;
@@ -15,7 +14,6 @@ export const AuthSwitchContainer = styled.p`
 export const AuthSwitchButton = styled.button`
   background-color: transparent;
   cursor: pointer;
-  color: var(--color-purple-secondary);
   padding: 0;
   font-weight: 700;
   border: none;
