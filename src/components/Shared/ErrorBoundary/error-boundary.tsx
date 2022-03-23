38235b9c48a20430638fe73467c777fc3826908f
Adeola <adeola.adeyemoj@yahoo.com>
2022-03-23 15:41:36 +0100

    update: create error boundary component
    Signed-off-by: Adeola <adeola.adeyemoj@yahoo.com>

---
+++ src/components/Shared/ErrorBoundary/error-boundary.tsx
@@ -0,0 +1,42 @@
+import { Component, ReactNode, ErrorInfo } from 'react';
+import { Link } from 'react-router-dom';
+import { ErrorBoundaryContainer } from './error-boundary.styled';
+
+type ErrorBoundaryProps = {
+  link: string;
+  children: ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  public state: ErrorBoundaryState = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render(): ReactNode {
+    const { children, link } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return (
+        <ErrorBoundaryContainer>
+          <p>Something went wrong</p>
+          <Link to={link}>Go back home</Link>
+        </ErrorBoundaryContainer>
+      );
+    }
+    return children;
+  }
+}
