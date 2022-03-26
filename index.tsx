e3f5299ef3c79b24341ec590e98a309f53f11438
Adeola <adeola.adeyemoj@yahoo.com>
2022-03-26 14:55:45 +0100

    chore: add sanitize css to client
    Signed-off-by: Adeola <adeola.adeyemoj@yahoo.com>

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import 'sanitize.css';
 
 import App from './App';
 
