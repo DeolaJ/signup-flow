8c380bccc6a631d586d9a9731c4262335929b680
Adeola <adeola.adeyemoj@yahoo.com>
2022-03-26 14:57:43 +0100

    feat: add selectors for the form reducer values
    Signed-off-by: Adeola <adeola.adeyemoj@yahoo.com>

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,6 +1,12 @@
-import { StateType, UserState } from '../../types';
+import { FormState, StateType, UserState } from '../../types';
 
+// User Selectors
 export const selectUsers = (state: StateType): UserState['users'] => state.user.users;
 export const selectUser = (state: StateType): UserState['user'] => state.user.user;
 export const selectIsLoggedIn = (state: StateType): UserState['isLoggedIn'] =>
   state.user.isLoggedIn;
+
+// Form selectors
+export const selectUserRoles = (state: StateType): FormState['roles'] => state.form.roles;
+export const selectModifiedUser = (state: StateType): FormState['userInfo'] | null =>
+  Object.keys(state.form.userInfo || {}).length > 0 ? state.form.userInfo : null;
